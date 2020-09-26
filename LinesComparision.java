2fd2565aece1f2a1593440cfdd457311650d6e90
unknown <divyacharaninabha@gmail.com>
2020-09-26 10:55:28 +0530

    Refractor: Refractored using class methods to compare lines

--- LinesComparision.java
+++ LinesComparision.java
@@ -1,6 +1,25 @@
 package com.bridgelabz.linesComparision;
 
 public class LinesComparision {
+	
+	public void compareTo(Double lineOne, Double lineTwo)
+	{
+		//Comparison of two lines using Java compareTo method
+		int comparedValue = lineOne.compareTo(lineTwo);
+		if(comparedValue>0)
+			System.out.println("The length of the line one is greater than length of line two");
+		else if(comparedValue<0)
+			System.out.println("The length of the line one is less than length of line two");
+	}
+	public void equals(Double lineOne, Double lineTwo)
+	{
+		//Checking of equality of two lines using Java equals method
+		boolean areEqual = lineOne.equals(lineTwo);
+		if(areEqual)
+			System.out.println("The length of the two lines are equal");
+		else 
+			System.out.println("The length of the two lines are not equal");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -32,14 +51,12 @@ public class LinesComparision {
 		Double lineTwoLength = Math.sqrt((Math.pow(xCoorDiff,2))+(Math.pow(yCoorDiff,2)));
 		System.out.println("Length of Line Two "+lineTwoLength+" units");
 		
-		//Comparison of two lines using Java compareTo method
-		int comparedValue = lineOneLength.compareTo(lineTwoLength);
-		if(comparedValue==0)
-			System.out.println("The length of the two lines are equal");
-		else if(comparedValue>0)
-			System.out.println("The length of the line one is greater than length of line two");
-		else if(comparedValue<0)
-			System.out.println("The length of the line one is less than length of line two");
+		//Comparison of lines
+		LinesComparision object = new LinesComparision();
+		object.equals(lineOneLength,lineTwoLength);
+		object.compareTo(lineOneLength,lineTwoLength);
+		
+		
 
 		
 	
