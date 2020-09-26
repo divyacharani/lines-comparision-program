3fe12e0311ded617d9f0c021bffa8a44b4403584
unknown <divyacharaninabha@gmail.com>
2020-09-26 10:33:16 +0530

    Added: Equality check of two lines added

--- LinesComparision.java
+++ LinesComparision.java
@@ -6,23 +6,40 @@ public class LinesComparision {
 		// TODO Auto-generated method stub
 
 		//Variables
-		int lineX1,lineX2,lineY1,lineY2;
-	    int xCoorDiff, yCoorDiff;
+		int lineOneX1,lineOneX2,lineOneY1,lineOneY2,lineTwoX1,lineTwoX2,lineTwoY1,lineTwoY2;
+		int xCoorDiff, yCoorDiff;
 				
 	    //Welcome message
 		System.out.println("Welcome to Line Comparison Computation Program");
 				
 		//Generating coordinate points using random
-		lineX1 = (int)(Math.floor(Math.random()*10));
-		lineX2 = (int)(Math.floor(Math.random()*10));
-		lineY1 = (int)(Math.floor(Math.random()*10));
-		lineY2 = (int)(Math.floor(Math.random()*10));
+		lineOneX1 = (int)(Math.floor(Math.random()*10));
+		lineOneX2 = (int)(Math.floor(Math.random()*10));
+		lineOneY1 = (int)(Math.floor(Math.random()*10));
+		lineOneY2 = (int)(Math.floor(Math.random()*10));
+		lineTwoX1 = (int)(Math.floor(Math.random()*10));
+		lineTwoX2 = (int)(Math.floor(Math.random()*10));
+		lineTwoY1 = (int)(Math.floor(Math.random()*10));
+		lineTwoY2 = (int)(Math.floor(Math.random()*10));
 				
-		//Computing length of the line
-		xCoorDiff = lineX1-lineX2;
-		yCoorDiff = lineY1-lineY2;	
-		double lineLength = Math.sqrt((Math.pow(xCoorDiff,2))+(Math.pow(yCoorDiff,2)));
-		System.out.println("Length of the line "+lineLength+" units");
+		//Computation length of the line
+		xCoorDiff = lineOneX1-lineOneX2;
+		yCoorDiff = lineOneY1-lineOneY2;
+		Double lineOneLength = Math.sqrt((Math.pow(xCoorDiff,2))+(Math.pow(yCoorDiff,2)));
+		System.out.println("Length of Line One "+lineOneLength+" units");
+		xCoorDiff = lineTwoX1-lineTwoX2;
+		yCoorDiff = lineTwoY1-lineTwoY2;
+		Double lineTwoLength = Math.sqrt((Math.pow(xCoorDiff,2))+(Math.pow(yCoorDiff,2)));
+		System.out.println("Length of Line Two "+lineTwoLength+" units");
+		
+		//Checking of equality of two lines using Java equality method
+		boolean areEqual = lineOneLength.equals(lineTwoLength);
+		if(areEqual)
+			System.out.println("The length of the two lines are equal");
+		else 
+			System.out.println("The length of the two lines are not equal");
+
+		
 	
 	}
 
