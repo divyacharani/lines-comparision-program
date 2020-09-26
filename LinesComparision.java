21173b65beda617294b559363b9720b19eb53224
unknown <divyacharaninabha@gmail.com>
2020-09-26 10:24:27 +0530

    Added: Welcome message added

---
+++ LinesComparision.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linesComparision;
+
+public class LinesComparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Welcome message
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
